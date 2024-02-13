fa2b35d28efb32c086cc8080187341bebd441c7f
Akshay <yuvi.akshay@gmail.com>
2024-02-13 11:05:43 -0800

    Initial commit

---
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,41 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.Question;
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    @DeleteMapping("delete")
+    public String deleteQuestion(@RequestBody Question question) {
+        return questionService.deleteQuestion(question);
+    }
+
+    @PutMapping("update")
+    public String updateQuestion(@RequestBody Question question) {
+        return questionService.updateQuestion(question);
+    }
+}
